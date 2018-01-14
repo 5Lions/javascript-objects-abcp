e3effc2ac0c182e9d16bf9c5099f5078a665ada0
A Lee <ayslee@gmail.com>
2018-01-14 06:31:21 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,4 @@
 var playlist = {
-  TheVerve : 'Sonnet'
 };
 
 playlist['Slowdive'] = 'Alison';
@@ -11,10 +10,10 @@ function updatePlaylist(playlist, artist, song) {
   playlist.artist = song;
   return playlist;
 }
-updatePlaylist()
-
+updatePlaylist(playlist, 'Phil Ochs', "Here's to the State of Mississippi");
 
 function removeFromPlaylist(playlist, artist) {
   delete playlist.artist;
   return playlist;
 }
+removeFromPlaylist(playlist, 'Phil Ochs');
